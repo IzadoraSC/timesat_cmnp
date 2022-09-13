49965b32e18e1bc99477ab5d7b49d9e17b15828e
IzadoraSC <izadoraflorestal@gmail.com>
2022-09-13 10:27:32 -0300

    Script for downloading data from AρρEEARS (e.g. MODIS-NDVI), using R, ok.

--- scripts/1_download_MOD13Q1.R
+++ scripts/1_download_MOD13Q1.R
@@ -1,6 +1,22 @@
-library(rTIMESAT)
-library(raster)
-library(phenofit)
+
+
+#### Load packages using the function library when required.
+library(curl)
+library(rgdal)
+library(rts)
+library(stringr)
+library(getPass)           # A micro-package for reading passwords
+library(httr)              # To send a request to the server/receive a response from the server
+library(jsonlite)          # Implements a bidirectional mapping between JSON data and the most important R data types
+library(geojsonio)         # Convert data from various R classes to 'GeoJSON' 
+library(geojsonR)          # Functions for processing GeoJSON objects
+library(rgdal)             # Functions for spatial data input/output
+library(sp)                # classes and methods for spatial data types
+library(raster)            # Classes and methods for raster data
+# library(rasterVis)         # Advanced plotting functions for raster objects 
+# library(ggplot2)           # Functions for graphing and mapping
+# library(RColorBrewer)      # Creates nice color schemes
+
 
 # Drought hazard assessment
 
@@ -10,7 +26,8 @@ library(phenofit)
 # Data path: Enter the path to a folder where you have enough free space to store your 
   # MODIS data and the resulting products. 
 
-dataPath <- "C:/Users/user/Documents/GitHub/timesat_cmnp/data/MOD13Q1_CMNP_2000-2022/"
+dataPath <- "C:/Users/user/Documents/GitHub/timesat_cmnp/data/test/"
+dir()
 # outDir <- file.path("R_output")               # Create an output directory if it doesn't exist
 # suppressWarnings(dir.create(outDir))                 
 
@@ -44,26 +61,7 @@ cloudmask <- 1
 
 #AnalysisPeriod <- c(2000, 2022)
 
-
-#### Load packages using the function library when required.
-library(raster)
-library(curl)
-library(rgdal)
-library(rts)
-library(stringr)
-library(getPass)           # A micro-package for reading passwords
-library(httr)              # To send a request to the server/receive a response from the server
-library(jsonlite)          # Implements a bidirectional mapping between JSON data and the most important R data types
-library(geojsonio)         # Convert data from various R classes to 'GeoJSON' 
-library(geojsonR)          # Functions for processing GeoJSON objects
-library(rgdal)             # Functions for spatial data input/output
-library(sp)                # classes and methods for spatial data types
-library(raster)            # Classes and methods for raster data
-library(rasterVis)         # Advanced plotting functions for raster objects 
-library(ggplot2)           # Functions for graphing and mapping
-library(RColorBrewer)      # Creates nice color schemes
-
-## Step 5. Download Data
+## Step 4.1 Download Data
 
 #### Download NDVI and Pixel Reliability data which was previously ordered from 
   # appEEARS. The text file contains all ordered granules (including ‘VI quality 
@@ -111,27 +109,10 @@ prettify(token_response)                                       # Print the prett
 # all_Prods <- toJSON(prods_content, auto_unbox = TRUE)    # Convert the info to JSON object
 # remove(prods_req, prods_content)                         # Remove the variables that are not needed anymore
 
-####Verificando
-#DAQUI ATÉ
-response <- POST(paste0(API_URL, "task"), body = task_json , encode = "json", 
-                 add_headers(Authorization = token, "Content-Type" = "application/json"))
-
-task_content <- content(response)                                     # Retrieve content of the request 
-task_response <- jsonlite::toJSON(task_content, auto_unbox = TRUE)    # Convert the content to JSON and prettify it
-prettify(task_response)                                               # Print the task response
-#############
-##Before downloading the request output, examine the files contained in the request output.
-
-task_id <- fromJSON(task_response)[[1]]
-response <- GET(paste0(API_URL, "bundle/", task_id), add_headers(Authorization = token))
-bundle_response <- prettify(toJSON(content(response), auto_unbox = TRUE))
 
 
-####
-
-#AQUI
-
-#### essa parte funcionou
+#### This API call will list all of the requests associated with your user account, 
+  # automatically sorted by date descending with the most recent requests listed first.
 token <- paste("Bearer", fromJSON(token_response)$token)     # Save login token to a variable
 
 
@@ -147,7 +128,8 @@ prettify(status_response)                                           # Print the 
 ## 
 ## By: https://appeears.earthdatacloud.nasa.gov/api/?language=r#download-file
 
-#### Essa parte funcionou bem
+#### This API call lists all of the files contained in the bundle which are available 
+  # for download.
 
 token <- paste("Bearer", fromJSON(token_response)$token) # Save login token to a variable
 task_id <- "559b7267-3cab-4e8e-a9d0-aa909565a41a"
@@ -155,21 +137,12 @@ response <- GET(paste("https://appeears.earthdatacloud.nasa.gov/api/bundle/", ta
 bundle_response <- prettify(toJSON(content(response), auto_unbox = TRUE))
 bundle_response
 
-###VERIFICAR DAQUI ATÉ
-
-# create a destination directory to store the file in
-dest_dir <- 'your-destination-directory'
-filepath <- paste(dest_dir, filename, sep = '/')
-suppressWarnings(dir.create(dirname(filepath)))
-
-# write the file to disk using the destination directory and file name 
-response <- GET(paste("https://appeears.earthdatacloud.nasa.gov/api/bundle/", task_id, '/', file_id, sep = ""),
-                write_disk(filepath, overwrite = TRUE), progress(), add_headers(Authorization = token))
-
-# AQUI
 ########
 
-### Essa parte deu certo e foi feita por ultimo
+### Download files.Each file in a bundle can be downloaded. Just as the task has a task_id to 
+  # identify it, each file in the bundle will also have a unique file_id which should be used 
+  # for any operation on that specific file.
+
 bundle <- fromJSON(bundle_response)$files
 for (id in bundle$file_id){
   # retrieve the filename from the file_id
@@ -186,7 +159,9 @@ for (id in bundle$file_id){
 
 ########################
 
-
+library(rTIMESAT)
+library(raster)
+library(phenofit)
 
 # library(raster)
 # 
